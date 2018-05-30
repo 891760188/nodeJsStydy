0049ffe4153a1bee785aff89cb95fbeb2aa614b1
891760188 <891760198@qq.com>
2018-05-30 16:14:32 +0800

    first commit code

---
+++ Model/User.js
@@ -0,0 +1,12 @@
+/**
+ * auth:黄国烨
+ * miai:448249687@qq.com
+ *desc:数据库模型
+ * */
+module.exports=User;
+function User(){
+    this.tableName='tb_test_user_';
+    this.id='id';
+    this.name='name';
+    this.password='password';
+}
